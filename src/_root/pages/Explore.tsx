028230e7c5b75640559ae4c1b5c347eb2b168573
Muhammad Kaif <83498731+mKaif1193@users.noreply.github.com>
2024-09-12 11:55:47 +0500

    Final Deployment

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -64,12 +64,6 @@ function Explore() {
 
         <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
           <p className="small-medium md:base-medium text-light-2">All</p>
-          {/* <img
-            src="/assets/icons/filter.svg"
-            alt="Filter Posts"
-            width={20}
-            height={20}
-          /> */}
         </div>
       </div>
 
@@ -83,7 +77,7 @@ function Explore() {
           <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
         ) : (
           posts.pages.map((item, index) => (
-            <GridPostList key={`page-${index}`} posts={item.documents} />
+            <GridPostList key={`page-${index}`} posts={item!.documents} />
           ))
         )}
       </div>
