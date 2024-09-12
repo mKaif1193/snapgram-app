2815ac9f8d48a52dd5f2e0de038197e364404079
Muhammad Kaif <83498731+mKaif1193@users.noreply.github.com>
2024-09-12 12:05:14 +0500

    Final Deploy

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -178,14 +178,15 @@ export const useDeletePost = () => {
 export const useGetPosts = () => {
   return useInfiniteQuery({
     queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-    queryFn: getInfinitePosts as any,
+    queryFn: getInfinitePosts,
+    initialPageParam: 0,
     getNextPageParam: (lastPage: any) => {
       if (lastPage && lastPage.documents.length === 0) {
         return null;
       }
 
-      const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
-      return lastId;
+      const lastId = lastPage?.documents[lastPage.documents.length - 1].$id;
+      return lastId || null;
     },
   });
 };
