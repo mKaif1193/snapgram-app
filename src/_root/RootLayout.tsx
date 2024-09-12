184fddc810a072ce91f9f170b30a3de52883cf36
Muhammad Kaif <83498731+mKaif1193@users.noreply.github.com>
2024-09-12 13:10:37 +0500

    Final Deployment

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,20 +1,28 @@
 import Bottombar from "@/components/shared/Bottombar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import Topbar from "@/components/shared/Topbar";
-import { Outlet } from "react-router-dom";
+import { useUserContext } from "@/context/AuthContext";
+import { Navigate, Outlet } from "react-router-dom";
 
 function RootLayout() {
+  const { isAuthenticated } = useUserContext();
   return (
-    <div className="w-full md:flex">
-      <Topbar />
-      <LeftSidebar />
-
-      <section className="flex flex-1 h-full">
-        <Outlet />
-      </section>
-
-      <Bottombar />
-    </div>
+    <>
+      {!isAuthenticated ? (
+        <Navigate to="/login" />
+      ) : (
+        <>
+          <div className="w-full md:flex">
+            <Topbar />
+            <LeftSidebar />
+            <section className="flex flex-1 h-full">
+              <Outlet />
+            </section>
+            <Bottombar />
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
